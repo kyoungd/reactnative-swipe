e0f5de48d35771907a12b612e2769fcdaca6c698
Young K <kyoungd@hotmail.com>
2019-05-09 19:06:39 -0700

    first commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# set emulator as environment variable and path.  Take a look at it in the control panel.
+emulator -list-avds
+emulator -avd [emulator-name]
+# for me, for now.
+emulator -avd Nexus_S_API_28
+
+npm start
+
+# install default styling for React Native.  Kind of like material-ui.
+npm install react-native-elements
---
+++ src/Deck.js
@@ -0,0 +1,164 @@
+import React, {Component} from 'react';
+import { 
+  StyleSheet, 
+  Animated, 
+  View, 
+  Text, 
+  PanResponder, 
+  Dimensions,
+  LayoutAnimation,
+  UIManager
+} from 'react-native';
+import { Card, Button } from 'react-native-elements';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = SCREEN_WIDTH * 0.5;
+const SWIPE_OUT_DURATION = 250;
+
+class Deck extends Component {
+  /*
+    These are the default props.
+    You can also use type to check empty variables, but this is easier.
+  */
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  }
+
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+
+    // onStartShouldSetPanResponder - returns true, than press is handled by this function.
+    // onPanResponderMove - handles when dragging
+    // onPanResponderRelease - handles when user releases dragging
+    const handleCardGesture = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({x: gesture.dx, y: gesture.y});
+      },
+      onPanResponderRelease: (event, gesture) => {
+        const index = this.state.cardIndex;
+        if (gesture.dx > SWIPE_THRESHOLD)
+          this.forceSwipe('right');
+        else if (gesture.dx < -SWIPE_THRESHOLD)
+          this.forceSwipe('left');
+        else
+          this.resetPosition();
+      }
+    });
+    this.state = { handleCardGesture, position, index: 0 };
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  /*
+    This method is called when component receives new data to draw.
+    It resets the index to 0.
+  */
+  componentWillReceiveProps(nextProps) {
+    if (newProps.data != this.props.data) {
+      this.setState({index: 0})
+    }
+  }
+
+  /*
+    This method smooths out the jump when a new card is shown after the previous one is swiped.
+    The state.index data set and this method is called.
+    This calls an overall broad stroke animation on the next re-draw.
+    LayoutAnimation.spring() gives it a spring bounce.
+  */
+  componentWillUpdate() {
+    UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring();
+  }
+
+  forceSwipe(direction) {
+    const width = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this.state.position, {
+      toValue: {x: width, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight } = this.props;
+    const item = this.props.data[this.state.index];
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({x:0, y:0});
+    this.setState({index: this.state.index + 1});
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
+      outputRange: ['-85deg', '0deg', '85deg']
+    });
+
+    return { 
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    }
+  }
+
+  renderCards() {
+    return this.props.data.map((item, index) => {
+      if (index === this.state.index) {
+        return (
+          <Animated.View 
+            key={item.id}
+            style = { [this.getCardStyle(), styles.card, {zIndex: index * -1}] }
+            {...this.state.handleCardGesture.panHandlers}
+          >
+            { this.props.renderCard(item) }
+          </Animated.View>
+        )
+      }
+      else if (index > this.state.index)
+        return (
+          <Animated.View 
+            key={item.id} 
+            style={[styles.card, {zIndex: index * -1, top: 10 * (index - this.state.index)}]}
+            >
+            { this.props.renderCard(item) }
+          </Animated.View>
+        ); 
+    })
+  }
+
+  renderNoMoreCard() {
+    return (
+      <Card title="All Done!" key="00">
+        <Text style={{ marginBottom: 10 }}>
+          There is no more content here!
+        </Text>
+        <Button
+          title="Get More" >
+        </Button>
+      </Card>
+    )
+  }
+
+  render() {
+    return (
+      <View>
+        { this.state.index < this.props.data.length ? this.renderCards() : this.renderNoMoreCard() }
+      </View>
+    )
+  }
+}
+
+const styles = {
+  card: {
+    position: 'absolute',
+    width: SCREEN_WIDTH,
+  }
+}
+export default Deck;
